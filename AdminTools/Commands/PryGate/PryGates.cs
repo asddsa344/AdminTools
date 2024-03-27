b8e221804332dfd1b22ec820335dfa63b89d945b
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-03-27 16:16:30 +0100

    revert

--- AdminTools/Commands/PryGate/PryGates.cs
+++ AdminTools/Commands/PryGate/PryGates.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AdminTools.Commands
+namespace AdminTools.Commands.PryGate
 {
     [CommandHandler(typeof(RemoteAdminCommandHandler))]
     [CommandHandler(typeof(GameConsoleCommandHandler))]

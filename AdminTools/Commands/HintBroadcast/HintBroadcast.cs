f67f4ee0c39a6bb7a06d9d8947a3810fff790640
6hundred9 <96420090+6hundred9@users.noreply.github.com>
2024-05-28 16:25:49 +0300

    HintBroadcast changes (#82)
    Co-authored-by: Nameless <85962933+Misfiy@users.noreply.github.com>

--- AdminTools/Commands/HintBroadcast/HintBroadcast.cs
+++ AdminTools/Commands/HintBroadcast/HintBroadcast.cs
@@ -1,250 +1,28 @@
 ﻿using CommandSystem;
-using Exiled.API.Features;
-using NorthwoodLib.Pools;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace AdminTools.Commands.HintBroadcast
 {
     [CommandHandler(typeof(RemoteAdminCommandHandler))]
     [CommandHandler(typeof(GameConsoleCommandHandler))]
-    public class HintBroadcast : ParentCommand, IUsageProvider
+    public class HintBroadcast : ParentCommand
     {
+        public HintBroadcast() => LoadGeneratedCommands();
+
         public override string Command { get; } = "hintbroadcast";
-
         public override string[] Aliases { get; } = new string[] { "hint" , "hbc" };
-
         public override string Description { get; } = "Broadcasts a message to either a user, a group, a role, all staff, or everyone";
 
-        public string[] Usage { get; } = new string[] { "Uhm" };
         public override void LoadGeneratedCommands()
         {
-            // RegisterCommand(new Add());
-            // RegisterCommand(new Clear());
-            // RegisterCommand(new Group());
-            // RegisterCommand(new User());
+            RegisterCommand(new Clear());
+            RegisterCommand(new Group());
+            RegisterCommand(new User());
         }
 
         protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response) // TODO: Make it ParentCommand
         {
-            if (!sender.CheckPermission(PlayerPermissions.Broadcasting, out response))
-                return false;
-
-            if (arguments.Count < 1)
-            {
-                response = "Usage:\nhint (time) (message)" +
-                    "\nhbc user (player id / name) (time) (message)" +
-                    "\nhbc group (group name) (time) (message)" +
-                    "\nhbc groups (list of groups (i.e.: owner,admin,moderator)) (time) (message)" +
-                    "\nhbc (random / someone) (time) (message)" +
-                    "\nhbc (staff / admin) (time) (message)" +
-                    "\nhbc clearall";
-                return false;
-            }
-
-            switch (arguments.At(0))
-            {
-                case "user":
-                    if (arguments.Count < 4)
-                    {
-                        response = "Usage: hbc user (player id / name) (time) (message)";
-                        return false;
-                    }
-
-                    Player ply = Player.Get(arguments.At(1));
-                    if (ply == null)
-                    {
-                        response = $"Player not found: {arguments.At(1)}";
-                        return false;
-                    }
-
-                    if (!ushort.TryParse(arguments.At(2), out ushort time) && time <= 0)
-                    {
-                        response = $"Invalid value for duration: {arguments.At(2)}";
-                        return false;
-                    }
-
-                    ply.ShowHint(Extensions.FormatArguments(arguments, 3), time);
-                    response = $"Hint sent to {ply.Nickname}";
-                    return true;
-                case "users":
-                    if (arguments.Count < 4)
-                    {
-                        response = "Usage: hbc users (player id / name group (i.e.: 1,2,3 or hello,there,hehe)) (time) (message)";
-                        return false;
-                    }
-
-                    string[] users = arguments.At(1).Split(',');
-                    List<Player> plyList = new();
-                    foreach (string s in users)
-                    {
-                        if (int.TryParse(s, out int id) && Player.Get(id) != null)
-                            plyList.Add(Player.Get(id));
-                        else if (Player.Get(s) != null)
-                            plyList.Add(Player.Get(s));
-                    }
-
-                    if (!ushort.TryParse(arguments.At(2), out ushort tme) && tme <= 0)
-                    {
-                        response = $"Invalid value for duration: {arguments.At(2)}";
-                        return false;
-                    }
-
-                    foreach (Player p in plyList)
-                        p.ShowHint(Extensions.FormatArguments(arguments, 3), tme);
-
-
-                    StringBuilder builder = StringBuilderPool.Shared.Rent("Hint sent to players: ");
-                    foreach (Player p in plyList)
-                    {
-                        builder.Append("\"");
-                        builder.Append(p.Nickname);
-                        builder.Append("\"");
-                        builder.Append(" ");
-                    }
-                    string message = builder.ToString();
-                    StringBuilderPool.Shared.Return(builder);
-                    response = message;
-                    return true;
-                case "group":
-                    if (arguments.Count < 4)
-                    {
-                        response = "Usage: hbc group (group) (time) (message)";
-                        return false;
-                    }
-
-                    UserGroup broadcastGroup = ServerStatic.PermissionsHandler.GetGroup(arguments.At(1));
-                    if (broadcastGroup == null)
-                    {
-                        response = $"Invalid group: {arguments.At(1)}";
-                        return false;
-                    }
-
-                    if (!ushort.TryParse(arguments.At(2), out ushort tim) && tim <= 0)
-                    {
-                        response = $"Invalid value for duration: {arguments.At(2)}";
-                        return false;
-                    }
-
-                    foreach (Player player in Player.List)
-                    {
-                        if (player.Group.BadgeText.Equals(broadcastGroup.BadgeText))
-                            player.ShowHint(Extensions.FormatArguments(arguments, 3), tim);
-                    }
-
-                    response = $"Hint sent to all members of \"{arguments.At(1)}\"";
-                    return true;
-                case "groups":
-                    if (arguments.Count < 4)
-                    {
-                        response = "Usage: hbc groups (list of groups (i.e.: owner,admin,moderator)) (time) (message)";
-                        return false;
-                    }
-
-                    string[] groups = arguments.At(1).Split(',');
-                    List<string> groupList = new();
-                    foreach (string s in groups)
-                    {
-                        UserGroup broadGroup = ServerStatic.PermissionsHandler.GetGroup(s);
-                        if (broadGroup != null)
-                            groupList.Add(broadGroup.BadgeText);
-
-                    }
-
-                    if (!ushort.TryParse(arguments.At(2), out ushort e) && e <= 0)
-                    {
-                        response = $"Invalid value for duration: {arguments.At(2)}";
-                        return false;
-                    }
-
-                    foreach (Player p in Player.List)
-                        if (groupList.Contains(p.Group.BadgeText))
-                            p.ShowHint(Extensions.FormatArguments(arguments, 3), e);
-
-
-                    StringBuilder bdr = StringBuilderPool.Shared.Rent("Hint sent to groups with badge text: ");
-                    foreach (string p in groupList)
-                    {
-                        bdr.Append("\"");
-                        bdr.Append(p);
-                        bdr.Append("\"");
-                        bdr.Append(" ");
-                    }
-                    string ms = bdr.ToString();
-                    StringBuilderPool.Shared.Return(bdr);
-                    response = ms;
-                    return true;
-                case "random":
-                case "someone":
-                    if (arguments.Count < 3)
-                    {
-                        response = "Usage: hbc (random / someone) (time) (message)";
-                        return false;
-                    }
-
-                    if (!ushort.TryParse(arguments.At(1), out ushort me) && me <= 0)
-                    {
-                        response = $"Invalid value for duration: {arguments.At(1)}";
-                        return false;
-                    }
-
-                    Player plyr = Player.List.ToList()[Main.NumGen.Next(0, Player.List.Count())];
-                    plyr.ShowHint(Extensions.FormatArguments(arguments, 2), me);
-                    response = $"Hint sent to {plyr.Nickname}";
-                    return true;
-                case "staff":
-                case "admin":
-                    if (arguments.Count < 3)
-                    {
-                        response = "Usage: hbc (staff / admin) (time) (message)";
-                        return false;
-                    }
-
-                    if (!ushort.TryParse(arguments.At(1), out ushort t))
-                    {
-                        response = $"Invalid value for hint broadcast time: {arguments.At(1)}";
-                        return false;
-                    }
-
-                    foreach (Player pl in Player.List)
-                    {
-                        if (pl.ReferenceHub.serverRoles.RemoteAdmin)
-                            pl.ShowHint($"<color=orange>[Admin Hint]</color> <color=green>{Extensions.FormatArguments(arguments, 2)} - {((CommandSender)sender).Nickname}</color>", t);
-                    }
-
-                    response = $"Hint sent to all currently online staff";
-                    return true;
-                case "clearall":
-                    if (arguments.Count != 1)
-                    {
-                        response = "Usage: hbc clearall";
-                        return false;
-                    }
-
-                    foreach (Player py in Player.List)
-                        py.ShowHint(" ");
-                    response = "All hints have been cleared";
-                    return true;
-                default:
-                    if (arguments.Count < 3)
-                    {
-                        response = "Usage: hbc (time) (message)";
-                        return false;
-                    }
-
-                    if (!ushort.TryParse(arguments.At(1), out ushort tm))
-                    {
-                        response = $"Invalid value for hint broadcast time: {arguments.At(0)}";
-                        return false;
-                    }
-
-                    foreach (Player py in Player.List)
-                        py.ShowHint(Extensions.FormatArguments(arguments, 2), tm);
-                    break;
-            }
-            response = "";
+            response = "Not a valid subcommand. Available subcommands: user, group, clear";
             return false;
         }
     }
--- AdminTools/Commands/HintBroadcast/User.cs
+++ AdminTools/Commands/HintBroadcast/User.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommandSystem;
+using Exiled.API.Features;
 
 namespace AdminTools.Commands.HintBroadcast
 {
-    internal class User
+    internal class User : ICommand
     {
+        public string Command { get; } = "user";
+        public string[] Aliases { get; } = Array.Empty<string>();
+        public string Description { get; } = "Sends a broadcast to multiple users";
+        
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, [UnscopedRef] out string response)
+        {
+            if (arguments.Count < 3)
+            {
+                response = "Usage: hbc user (player ids / usernames) (time) (message)";
+                return false;
+            }
+
+            IEnumerable<Player> ply = Player.GetProcessedData(arguments);
+            if (ply.IsEmpty())
+            {
+                response = $"Player not found: {arguments.At(0)}";
+                return false;
+            }
+
+            if (!ushort.TryParse(arguments.At(1), out ushort time) && time <= 0)
+            {
+                response = $"Invalid value for duration: {arguments.At(1)}";
+                return false;
+            }
+
+            foreach (Player player in ply)
+            {
+                player.ShowHint(Extensions.FormatArguments(arguments, 2), time);
+            }
+
+            response = $"Hint sent to players";
+            return true;
+        }
     }
 }

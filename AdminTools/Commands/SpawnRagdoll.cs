119a8480f4cc65fd8fa940092715b269c2d81fcc
Benjamin <benjaminro21@gmail.com>
2024-05-31 05:29:21 -0500

    Gives SpawnRagdoll the ability to have custom names & Death reasons (#84)
    l

--- AdminTools/Commands/SpawnRagdoll.cs
+++ AdminTools/Commands/SpawnRagdoll.cs
@@ -5,6 +5,8 @@ using MEC;
 using System;
 using System.Collections.Generic;
 using PlayerRoles;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace AdminTools.Commands
 {
@@ -18,19 +20,25 @@ namespace AdminTools.Commands
 
         public string Description { get; } = "Spawns a specified number of ragdolls on a user";
 
-        public string[] Usage { get; } = new string[] { "%player%", "%role%", "amount" };
+        public string[] Usage { get; } = new string[] { "%player%", "%role%", "amount", "\"name\"", "\"death reason\"" };
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
+            string[] quotedArgs = Regex.Matches(string.Join(" ", arguments), "[^\\s\"\']+|\"([^\"]*)\"|\'([^\']*)\'")
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray()
+                .ToArray();
+
             if (!sender.CheckPermission("at.dolls"))
             {
                 response = "You do not have permission to use this command";
                 return false;
             }
 
-            if (arguments.Count != 3)
+            if (quotedArgs.Count() != 5 || quotedArgs.Count() != 3)
             {
-                response = "Usage: spawnragdoll ((player id / name) or (all / *)) (RoleTypeId) (amount)";
+                response = "Usage: spawnragdoll ((player id / name) or (all / *)) (RoleTypeId) (amount) (nameRagdoll) (deathReason)";
                 return false;
             }
 
@@ -53,21 +61,36 @@ namespace AdminTools.Commands
                 return false;
             }
 
+            string ragdollName = "SCP-343";
+            string deathReason = "End of the Universe";
+
+            if (quotedArgs.Count() == 5)
+            {
+                ragdollName = quotedArgs.ElementAt(3).Trim('\"');
+                deathReason = quotedArgs.ElementAt(4).Trim('\"');
+            }    
+
+            if(string.IsNullOrEmpty(ragdollName) || string.IsNullOrEmpty(deathReason))
+            {
+                ragdollName = "SCP-343";
+                deathReason = "End of the universe";
+            }
+
             foreach (Player player in players)
             {
-                Timing.RunCoroutine(SpawnDolls(player, type, amount));
+                Timing.RunCoroutine(SpawnDolls(player, type, amount, ragdollName, deathReason));
             }
 
             response = $"{amount} {type} ragdoll(s) have been spawned on {arguments.At(0)}.";
             return true;
         }
 
-        private IEnumerator<float> SpawnDolls(Player player, RoleTypeId type, int amount)
+        private IEnumerator<float> SpawnDolls(Player player, RoleTypeId type, int amount, string ragdollName, string deathReason)
         {
             for (int i = 0; i < amount; i++)
             {
                 if (player.IsAlive)
-                    Ragdoll.CreateAndSpawn(type, "SCP-343", "End of the Universe", player.Position, default);
+                    Ragdoll.CreateAndSpawn(type, ragdollName, deathReason, player.Position, default);
                 yield return Timing.WaitForSeconds(0.5f);
             }
         }
